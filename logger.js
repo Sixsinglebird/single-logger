4f2621ef11cdb5746d7b310f6d8bb81a0eac7292
Sixsinglebird <97618579+Sixsinglebird@users.noreply.github.com>
2023-08-22 16:34:32 -0230

    update logger.js

--- logger.js
+++ logger.js
@@ -16,14 +16,14 @@ const logEvent = async (event, level, message) => {
   // format does make it more legible
   const date = format(new Date(), "HH:mm:ss");
   const logItem = `${uuid()}\t${date}\t${level}\t${event}\t${message}`;
-  if (DEBUG) console.log(logItem);
+  // if (DEBUG) console.log(logItem);
   try {
     const logDir =
       "../../logs/" + getYear(new Date()) + "/" + getMonth(new Date()) + "/";
     if (!fs.existsSync(path.join(__dirname, logDir))) {
       // this is fun... without the method {recursive: true }
       //  mkdir will not create nested loops
-      if (DEBUG) console.log("Directory made");
+      // if (DEBUG) console.log("Directory made");
       await promise.mkdir(path.join(__dirname, logDir), { recursive: true });
       emitEvent.emit("log", "logger", "WARNING", "New Directory Made.");
     }
